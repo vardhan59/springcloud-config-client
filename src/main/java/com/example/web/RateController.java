e4e6c208458975edad5e520a51ed490910d0f8c8
vardhan59 <gorantlavishnu@gmail.com>
2017-03-07 14:33:22 +0530

    client

---
+++ src/main/java/com/example/web/RateController.java
@@ -0,0 +1,26 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RateController {
+	
+	@Value("${rate}")
+	String rate;
+	@Value("${lanecount}")
+	String laneCount;
+	@Value("${tollstart}")
+	String tollStart;
+
+	//http://localhost:8080/rate
+	@RequestMapping("/rate")
+	public String getRate(Model m) {
+		m.addAttribute("rate", rate);
+		m.addAttribute("lanes", laneCount);
+		m.addAttribute("tollStart", tollStart);
+		return "rateview";
+	}
+}
