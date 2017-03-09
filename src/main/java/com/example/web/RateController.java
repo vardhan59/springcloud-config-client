6b435ebf9b4688496c497187cdeb8dbef8d13db8
vardhan59 <gorantlavishnu@gmail.com>
2017-03-09 11:35:32 +0530

    push changes

--- src/main/java/com/example/web/RateController.java
+++ src/main/java/com/example/web/RateController.java
@@ -1,11 +1,13 @@
 package com.example.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RefreshScope
 public class RateController {
 	
 	@Value("${rate}")
